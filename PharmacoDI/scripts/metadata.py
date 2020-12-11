16d1da96bbda5f196d39967e64999f02d3360c01
EvgeniyaGorobets <evgeniya.a.gorobets@gmail.com>
2020-12-11 04:02:56 +0000

    Rewrote functions to build target and drug target tables

--- PharmacoDI/scripts/metadata.py
+++ PharmacoDI/scripts/metadata.py
@@ -3,7 +3,11 @@ import re
 import glob
 import numpy as np
 import pandas as pd
+import dask.dataframe as dd
+from join_pset_tables import *
 
+read_file_path = os.path.join('data', 'procdata')
+write_file_path = os.path.join('data', 'latest')
 metadata_path = os.path.join('data', 'metadata')
 annotations_path = os.path.join(metadata_path, 'Annotations')
 
@@ -129,24 +133,18 @@ def melt_and_join(meta_df, unique_id, join_df):
 drugbank_file = "drugbank_targets_has_ref_has_uniprot.csv"
 uniprot_ensemble_file = "uniprot_ensembl_mappings.csv"
 
-#TODO - generalize so it goes through all targets (ChEMBL, etc.)
-# TODO - clean this up, too many parameters?
-def build_target_dfs(drugbank_file, uniprot_ensemble_file, metadata_path, metadata_dfs):
-    # Get metadata
-    drugbank_targets_df = get_annotations(drugbank_file, metadata_path)
-    gene_mappings_df = get_annotations(uniprot_ensemble_file, metadata_path)
 
-    # Build targets table
-    #TODO - get gene table
-    metadata_dfs['target'] = build_target_df(drugbank_targets_df, gene_mappings_df)
+def build_metadata_dfs(join_dfs, uniprot_ensemble_file, metdata_path):
+    gene_mappings_df = get_metadata(uniprot_ensemble_file, metadata_path)
+    target_file_path = os.path.join(metadata_path, 'target')
+    target_df = build_target_df(target_file_path, gene_mappings_df, join_dfs['gene'])
+    #drug_target_df = build_drug_target_df(target_df, join_dfs['drug'])
+        #TODO: generalize drug-target mappings
 
-    # Build drug_targets table
-    #TODO - get drug table
-    metadata_dfs['drug_targets'] = build_drug_targets_df(drugbank_targets_df, metadata_dfs['target'], None)
 
 
 #TODO - generalize so it goes through all targets (ChEMBL, etc.)
-def build_target_df(drugbank_targets_df, gene_mappings_df):
+def build_target_df(target_file_path, gene_mappings_df, gene_df):
     """
     Map all Drugbank (TODO - generalize) targets to EnsemblGene IDs
     based on UniProt IDs (TODO - join with gene table?).
@@ -155,53 +153,45 @@ def build_target_df(drugbank_targets_df, gene_mappings_df):
     @param gene_mappings_df: [`DataFrame`] A table mapping UniProt IDs to EnsemlGene IDs
     @return: [`DataFrame`] A table of target to gene mappings.
     """
-    
-    # Get all targets & UniProtIds from Drugbank
-    target_df = drugbank_targets_df[['name', 'polypeptide.external.identifiers.UniProtKB']].drop_duplicates()
+    # Get all target files (Drugbank, ChemBL, etc.)
+    target_files = glob.glob(target_file_path)
+    if len(target_files) == 0:
+        raise ValueError(
+            f'No target directory named {target_file_path} could be found!')
+
+    # Load all target tables
+    dfs = [] 
+    for target_file in os.listdir(target_files):
+        df = (get_metadata(target_file, target_file_path)) #change so that it uses dask to load
+        dfs.append(df[['name', 'polypeptide.external.identifiers.UniProtKB']])
+
+    # Append all target tables to each other
+    target_df = concat_tables(dfs)
     
     # Rename columns for easier merging
     target_df.rename(columns={'polypeptide.external.identifiers.UniProtKB': 'UniProtId'}, inplace=True)
     
-    # Join on UniProt ID to get EnsemblGene IDs
+    # Join on UniProt ID to get EnsemblGene IDs (1300 rows)
     target_df = pd.merge(target_df, gene_mappings_df, on='UniProtId', how='inner')
-    
-    # Drop UniProtId and rename gene column
     target_df.drop('UniProtId', axis='columns', inplace=True)
     target_df.rename(columns={'EnsemblGeneId': 'gene_id'}, inplace=True)
-    
-    # Re-index to start at 1
-    target_df.index = target_df.index + 1
+
+    # Join with gene table
+    target_df = safe_merge(target_df, gene_df, 'gene_id')
     
     return target_df
 
 
-#TODO - generalize, and make cleaner(?)
-def build_drug_targets_df(drugbank_targets_df, target_df, drug_df):
-
+def build_drug_target_df(drugbank_target_df, target_df, drug_df):
     # Get all drug-target mappings from Drugbank
-    drug_targets_df = drugbank_targets_df[['name', 'drugName']].drop_duplicates()
+    drug_target_df = drugbank_target_df[['name', 'drugName']].drop_duplicates()
+    drug_target_df.rename(columns={'name': 'target_id', 'drugName': 'drug_id'}, inplace=True)
 
-    # Target df with index and target name only
-    # find a better way to do this? or just make sure everything has an 'id' column?
-    targets = target_df[['name']]
-    targets['id'] = targets.index
+    # Build foreign keys by joining with drug table and target table
+    drug_target_df = safe_merge(drug_target_df, drug_df, 'drug_id')
+    drug_target_df = safe_merge(drug_target_df, target_df, 'target_id')
 
-    # Join with target_df
-    drug_targets_df = pd.merge(drug_targets_df, targets, on='name', how='inner')
-
-    # Drop target name column and rename columns
-    drug_targets_df.drop('name', axis='columns', inplace=True)
-    drug_targets_df.rename(columns={'drugName': 'name', 'id': 'target_id'}, inplace=True)
-
-    # Join with drug_df
-    drug_targets_df = pd.merge(drug_targets_df, drug_df, on='name', how='inner')
-
-    # Drop drug name column and rename columns
-    drug_targets_df.drop('name', axis='columns', inplace=True)
-    drug_targets_df.rename(columns={'id': 'drug_id'}, inplace=True)
-
-    # Reset index
-    drug_targets_df.reset_index(drop=True, inplace=True)
-    drug_targets_df.index = drug_targets_df.index + 1
-
-    return drug_targets_df
+    # Re-index
+    drug_target_df = reindex_table(drug_target_df)
+    
+    return drug_target_df
