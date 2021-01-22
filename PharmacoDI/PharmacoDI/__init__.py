95f042df5f796681b34d50357021b83f51811ec1
EvgeniyaGorobets <evgeniya.a.gorobets@gmail.com>
2021-01-22 20:45:00 +0000

    Added imports for all essential functions

--- PharmacoDI/PharmacoDI/__init__.py
+++ PharmacoDI/PharmacoDI/__init__.py
@@ -3,4 +3,11 @@ from .download_canonical_psets import *
 from .download_gene_signatures import *
 from .get_gene_targets import get_gene_targets
 from .get_target_annotations import get_target_annotations, query_uniprot_mapping_api
-from .read_pset import *
+from .read_pset import pset_df_to_nested_dict, read_pset_file, read_pset
+from .build_all_pset_tables import build_all_pset_tables
+from .get_chembl_targets import get_chembl_targets, target_file
+from .get_chembl_drug_targets import get_chembl_drug_target_mappings, drug_target_file
+from .build_target_tables import build_target_tables
+from .combine_pset_tables import combine_all_pset_tables
+from .build_synonym_tables import build_cell_synonym_df, build_drug_synonym_df, build_tissue_synonym_df
+from .build_cellosaurus import build_cellosaurus_df, cellosaurus_path
