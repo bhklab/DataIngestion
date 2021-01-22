adbd26c98475d0cddb6eee97e95aaad4eadbee3b
EvgeniyaGorobets <evgeniya.a.gorobets@gmail.com>
2021-01-22 14:02:12 +0000

    Renamed join_pset_tables.py to combine_pset_tables.py and renamed functions to use verb 'combine' instead of 'build'

--- PharmacoDI/scripts/combine_pset_tables.py
+++ PharmacoDI/scripts/combine_pset_tables.py
@@ -10,6 +10,128 @@ output_dir = os.path.join('data', 'demo')
 
 # TODO: change printed errors to actual errors
 
+# TODO - better name
+
+
+def combine_all_pset_tables(data_dir, output_dir):
+    join_dfs = combine_primary_tables(data_dir, output_dir)
+    join_dfs = combine_secondary_tables(join_dfs, data_dir, output_dir)
+    join_dfs = combine_experiment_tables(join_dfs, data_dir, output_dir)
+    return join_dfs
+
+
+def combine_primary_tables(data_dir, output_dir):
+    """
+    Build all the primary tables, i.e., tables that require no joins,
+    and return them in a dictionary.
+
+    @param data_dir: [`string`] The file path to read the PSet tables
+    @param output_dir: [`string`] The file path to write the final tables
+    @return: [`dict(string: datatable.Frame)`] A dictionary of all the primary
+                                                tables, with names as keys
+    """
+    # Load, concatenate, and write primary tables to disk
+    tissue_df = load_join_write('tissue', data_dir, output_dir)
+    drug_df = load_join_write('drug', data_dir, output_dir)
+    gene_df = load_join_write('gene', data_dir, output_dir)
+    dataset_df = load_join_write('dataset', data_dir, output_dir)
+
+    # Transform tables to be used for joins
+    dfs = {}
+    dfs['tissue'] = rename_and_key(tissue_df, 'tissue_id')
+    dfs['drug'] = rename_and_key(drug_df, 'drug_id')
+    dfs['gene'] = rename_and_key(gene_df, 'gene_id')
+    dfs['dataset'] = rename_and_key(dataset_df, 'dataset_id')
+    return dfs
+
+
+def combine_secondary_tables(data_dir, output_dir, join_dfs):
+    """
+    Build all secondary tables, i.e., all tables that have foreign keys corresponding
+    to primary keys of primary tables. The function reads PSet tables from 
+    data_dir, concatenates and joins them with tables from join_dfs, and 
+    writes them to output_dir.
+
+    @param join_dfs: [`dict(string: datatable.Frame)`] A dictionary of all the primary
+                                                    tables, with names as keys
+    @param data_dir: [`string`] The file path to read the PSet tables
+    @param output_dir: [`string`] The file path to write the final tables
+    @return: [`dict(string: datatable.Frame)`] The updated dictionary of join tables
+    """
+    # Build cell table and add to join_dfs dictionary
+    cell_df = load_join_write(
+        'cell', data_dir, output_dir, ['tissue'], join_dfs)
+    join_dfs['cell'] = rename_and_key(cell_df, 'cell_id')
+
+    # Build annotation tables
+    load_join_write('drug_annotation', data_dir,
+                    output_dir, ['drug'], join_dfs)
+    load_join_write('gene_annotation', data_dir,
+                    output_dir, ['gene'], join_dfs)
+
+    # Build all other secondary tables
+    load_join_write('dataset_cell', data_dir, output_dir,
+                    ['dataset', 'cell'], join_dfs)
+    load_join_write('mol_cell', data_dir, output_dir,
+                    ['cell', 'dataset'], join_dfs)
+    # mol_cells has Kallisto. not sure why. from CTRPv2 (TODO)
+    load_join_write('dataset_statistics', data_dir,
+                    output_dir, ['dataset'], join_dfs)
+    load_join_write('gene_drug', data_dir, output_dir, [
+                    'gene', 'drug', 'dataset', 'tissue'], join_dfs)
+
+    return join_dfs
+
+
+def combine_experiment_tables(join_dfs, data_dir, output_dir):
+    """
+    Load and process experiment table, then use it to build the dose response
+    and profile tables. Drop the 'name' column from the experiment table before
+    writing to a CSV.
+
+    @param join_dfs: [`dict(string: datatable.Frame)`]
+    @param data_dir: [`string`] The file path to the PSet tables
+    @param output_dir: [`string`] The file path to the final tables
+    @return: [`None`]
+    """
+    # Load all experiments from PSets
+    experiment_df = load_join_write('experiment', data_dir, output_dir, [
+                                    'cell', 'drug', 'dataset', 'tissue'], join_dfs)
+    # Don't write the 'name' column
+    experiment_df[:, ['id', 'cell_id', 'drug_id', 'dataset_id', 'tissue_id']].to_csv(
+        os.path.join(output_dir, 'experiment.csv'))
+
+    # Rename columns and key experiment table based on experiment name and dataset id
+    experiment_df.names = {'name': 'experiment_id'}
+    experiment_df = experiment_df[:, ['id', 'experiment_id', 'dataset_id']]
+    experiment_df.key = ('dataset_id', 'experiment_id')
+    join_dfs['experiment'] = experiment_df
+
+    # Nearly the same code as in load_join_write but has special case handling
+    for df_name in ['dose_response', 'profile']:
+        df = load_table(df_name, data_dir)
+        for fk in ['dataset', 'experiment']:
+            df = join_tables(df, join_dfs[fk], fk+'_id')
+        del df[:, 'dataset_id']
+        index_and_write(df, df_name, output_dir)
+
+    return join_dfs
+
+
+def load_join_write(name, data_dir, output_dir, foreign_keys=[], join_dfs=None):
+    df = load_table(name, data_dir)
+    if foreign_keys and not join_tables:
+        print(f'ERROR: The {name} table has foreign keys {foreign_keys}'
+              'but you have not passed any join_tables.')
+        return None
+
+    for fk in foreign_keys:
+        df = join_tables(df, join_dfs[fk], fk+'_id')
+
+    df = index_and_write(df, name, output_dir)
+
+    return df
+
 
 def load_table(name, data_dir):
     """
@@ -74,149 +196,17 @@ def join_tables(df1, df2, join_col):
 
 
 def index_and_write(df, name, output_dir):
+    """
+    Add a primary key to df ('id' column) and write it to output_dir
+    as a .csv file.
+
+    @param df: [`datatable.Frame`] A PharmacoDB table
+    @param name: [`string`] The name of the table
+    @param output_dir: [`string`] The directory to write the table to
+    @return: [`datatable.Frame`] The indexed PharmacoDB table
+    """
     # Index datatable
     df = cbind(dt.Frame(id=np.arange(df.nrows) + 1), df)
     # Write to .csv
     df.to_csv(os.path.join(output_dir, f'{name}.csv'))
     return df
-
-
-def load_join_write(name, data_dir, output_dir, foreign_keys=[], join_dfs=None):
-    df = load_table(name, data_dir)
-    if foreign_keys and not join_tables:
-        print(f'ERROR: The {name} table has foreign keys {foreign_keys}'
-              'but you have not passed any join_tables.')
-        return None
-
-    for fk in foreign_keys:
-        df = join_tables(df, join_dfs[fk], fk+'_id')
-
-    df = index_and_write(df, name, output_dir)
-
-    return df
-
-
-# TODO - similar names to build_pset_tables, should I change it to be clearer?
-def build_primary_tables(data_dir, output_dir):
-    """
-    Build all the primary tables, i.e., tables that require no joins,
-    and return them in a dictionary.
-
-    @param data_dir: [`string`] The file path to read the PSet tables
-    @param output_dir: [`string`] The file path to write the final tables
-    @return: [`dict(string: datatable.Frame)`] A dictionary of all the primary
-                                                tables, with names as keys
-    """
-    # Load, concatenate, and write primary tables to disk
-    tissue_df = load_join_write('tissue', data_dir, output_dir)
-    drug_df = load_join_write('drug', data_dir, output_dir)
-    gene_df = load_join_write('gene', data_dir, output_dir)
-    dataset_df = load_join_write('dataset', data_dir, output_dir)
-
-    # Transform tables to be used for joins
-    dfs = {}
-    dfs['tissue'] = rename_and_key(tissue_df, 'tissue_id')
-    dfs['drug'] = rename_and_key(drug_df, 'drug_id')
-    dfs['gene'] = rename_and_key(gene_df, 'gene_id')
-    dfs['dataset'] = rename_and_key(dataset_df, 'dataset_id')
-    return dfs
-
-
-def build_secondary_tables(join_dfs, data_dir, output_dir):
-    """
-    Build all secondary tables, i.e., all tables that have foreign keys corresponding
-    to primary keys of primary tables. The function reads PSet tables from 
-    data_dir, concatenates and joins them with tables from join_dfs, and 
-    writes them to output_dir.
-
-    @param join_dfs: [`dict(string: datatable.Frame)`] A dictionary of all the primary
-                                                    tables, with names as keys
-    @param data_dir: [`string`] The file path to read the PSet tables
-    @param output_dir: [`string`] The file path to write the final tables
-    @return: [`dict(string: datatable.Frame)`] The updated dictionary of join tables
-    """
-    # Build cell table and add to join_dfs dictionary
-    cell_df = load_join_write(
-        'cell', data_dir, output_dir, ['tissue'], join_dfs)
-    join_dfs['cell'] = rename_and_key(cell_df, 'cell_id')
-
-    # Build annotation tables
-    load_join_write('drug_annotation', data_dir,
-                    output_dir, ['drug'], join_dfs)
-    load_join_write('gene_annotation', data_dir,
-                    output_dir, ['gene'], join_dfs)
-
-    # Build all other secondary tables
-    load_join_write('dataset_cell', data_dir, output_dir,
-                    ['dataset', 'cell'], join_dfs)
-    load_join_write('mol_cell', data_dir, output_dir,
-                    ['cell', 'dataset'], join_dfs)
-    # mol_cells has Kallisto. not sure why. from CTRPv2
-    load_join_write('dataset_statistics', data_dir,
-                    output_dir, ['dataset'], join_dfs)
-    load_join_write('gene_drug', data_dir, output_dir, [
-                    'gene', 'drug', 'dataset', 'tissue'], join_dfs)
-
-    return join_dfs
-
-
-def build_experiment_tables(join_dfs, data_dir, output_dir):
-    """
-    Load and process experiment table, then use it to build the dose response
-    and profile tables. Drop the 'name' column from the experiment table before
-    writing to a CSV.
-
-    @param join_dfs: [`dict(string: datatable.Frame)`]
-    @param data_dir: [`string`] The file path to the PSet tables
-    @param output_dir: [`string`] The file path to the final tables
-    @return: [`None`]
-    """
-    # Load all experiments from PSets
-    experiment_df = load_join_write('experiment', data_dir, output_dir, [
-                                    'cell', 'drug', 'dataset', 'tissue'], join_dfs)
-    # Don't write the 'name' column
-    experiment_df[:, ['id', 'cell_id', 'drug_id', 'dataset_id', 'tissue_id']].to_csv(
-        os.path.join(output_dir, 'experiment.csv'))
-
-    # Rename columns and key experiment table based on experiment name and dataset id
-    experiment_df.names = {'name': 'experiment_id'}
-    experiment_df = experiment_df[:, ['id', 'experiment_id', 'dataset_id']]
-    experiment_df.key = ('dataset_id', 'experiment_id')
-    join_dfs['experiment'] = experiment_df
-
-    # Nearly the same code as in load_join_write but has special case handling
-    for df_name in ['dose_response', 'profile']:
-        df = load_table(df_name, data_dir)
-        for fk in ['dataset', 'experiment']:
-            df = join_tables(df, join_dfs[fk], fk+'_id')
-        del df[:, 'dataset_id']
-        index_and_write(df, df_name, output_dir)
-
-    return join_dfs
-
-
-# TODO - better name
-def build_all_tables(data_dir, output_dir):
-    join_dfs = build_primary_tables(data_dir, output_dir)
-    join_dfs = build_secondary_tables(join_dfs, data_dir, output_dir)
-    join_dfs = build_experiment_tables(join_dfs, data_dir, output_dir)
-    return join_dfs
-
-
-# Duplicate experiment_ids
-"""
-   | name                      count
--- + ------------------------  -----
- 0 | drugid_AZD6244_AU565          2
- 1 | drugid_AZD6244_HCC1187        2
- 2 | drugid_AZD6244_HCC1806        2
- 3 | drugid_AZD6244_HCC1954        2
- 4 | drugid_AZD6244_MCF7           2
- 5 | drugid_Erlotinib_HCC70        2
- 6 | drugid_Nilotinib_AU565        2
- 7 | drugid_Nilotinib_HCC1187      2
- 8 | drugid_Nilotinib_HCC1806      2
- 9 | drugid_Nilotinib_HCC1954      2
-10 | drugid_Nilotinib_MCF7         2
-11 | drugid_Sorafenib_HCC70        2
-"""
